116a970cd1d8012569a3cfb16658c710167ef5ad
Renato Cordova <ren99cordova@gmail.com>
2023-01-10 21:11:15 -0500

    finished most of the project, all controls work fine, the handlebars work as well, I styled them enough to make it pleasant to the eye. Log in and sign up functionality work fine. Next task is to find any bugs in the project

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,3 +22,54 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render("blog", {
+      ...blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("dashboard", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
